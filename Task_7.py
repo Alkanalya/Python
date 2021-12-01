33d29f592e308d1c033cfc07ccb8db8589b58bfa
san-s <sansapersh@gmail.com>
2021-12-01 18:01:25 +0300

    Исправление 7 задачи 5 урока

--- Task_7.py
+++ Task_7.py
@@ -1,14 +1,21 @@
-from itertools import count
+import json
 
-n = int(input('Введите число\n'))
-print()
+dict_with_profit = {}
+dict_with_lose = {}
+avarage_profit = {}
+with open("texts/task_7.txt", 'r', encoding='utf-8') as file:
+  for line in file:
+    data = line.split()
+    profit = int(data[2]) - int(data[3])
+    if profit >= 0:
+      dict_with_profit.update({data[0] : profit})
+    else:
+      dict_with_lose.update({data[0] : profit})
+  avarage_profit.update({'avarage_profit' : (sum(dict_with_profit.values())) // len(dict_with_profit.values())})
+  
+  total_data = [dict_with_profit, avarage_profit, dict_with_lose]
 
-def fuct():
-  global n
-  res = 1
-  for i in range(1,n+1):
-    res = res * i
-    yield res
+print(total_data)
 
-for el in fuct():
-    print(el)
+with open("texts/task_7.json", 'w', encoding='utf-8') as file_json:
+  json.dump(total_data, file_json, ensure_ascii=False)
